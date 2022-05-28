7b6e71047c8efaef024cb250f5c14d23b656ecb4
forsakens0ul <2514564882@qq.com>
2022-05-28 17:56:43 +0800

    1
    1

---
+++ portfolio-master/src/components/Navbar/navData.js
@@ -0,0 +1,33 @@
+export const navLinks = [
+    {
+        label: '成果展示',
+        pathname: '/',
+        hash: '#project-1',
+    },
+    {
+        label: '关于我们',
+        pathname: '/',
+        hash: '#details',
+    },
+    // {
+    //   label: 'Articles',
+    //   pathname: '/articles',
+    // },
+    {
+        label: '联系我们',
+        pathname: '/contact',
+    },
+]
+
+export const socialLinks = [
+    {
+        label: '邮箱',
+        url: 'mailto://1518625838@qq.com',
+        icon: 'mail',
+    },
+    {
+        label: 'Github',
+        url: '',
+        icon: 'github',
+    },
+]
